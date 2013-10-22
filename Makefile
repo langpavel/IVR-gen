224c471b0a3f6a28b7f0be3bb451389f83f01ab9
Pavel Lang <langpavel@maternacz.com>
2013-10-22 20:13:48 +0200

    Ecosystem

---
+++ Makefile
@@ -0,0 +1,13 @@
+
+all: build/ivr-explain.js
+
+clean:
+	$(RM) build/ivr-explain.js
+
+test:
+	node ./test/explain/test.js
+
+build/ivr-explain.js: grammar/ivr-explain.grammar
+	./node_modules/.bin/pegjs ./grammar/ivr-explain.grammar ./build/ivr-explain.js
+
+.PHONY: all clean test
---
+++ test/explain/test.js
@@ -0,0 +1,13 @@
+
+var fs = require('fs');
+var parser = require(__dirname + '/../../build/ivr-explain');
+
+
+var result = parser.parse(fs.readFileSync(__dirname + '/../../test.ivr', 'utf8'), 'GOON');
+
+result.code.forEach(function(x) { console.log(x) });
+
+if (result.errors.length) {
+  console.error("THERE ARE ERRORS:");
+  result.errors.forEach(function(x) { console.error(x); });
+}
